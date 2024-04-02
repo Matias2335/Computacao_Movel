18b4d58dd430d6d71a07865b3688ba393f5e9c1d
Matias2335 <113120652+Matias2335@users.noreply.github.com>
2024-04-02 17:23:23 +0000

    Definir as portas dos botoes

--- main.c
+++ main.c
@@ -1,4 +1,18 @@
-#include <stdio.h>
+#include <LiquidCrystal.h>
+#define Botao_Iniciar 11;
+#define Botao_Sim 10;
+#define Botao_Nao 9;
+#define Botao_Pular 8;
+
+LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+
+
+void setup(){
+  pinMode(Botao_Iniciar, INPUT_PULLUP);
+  pinMode(Botao_Sim, INPUT_PULLUP);
+  pinMode(Botao_Nao, INPUT_PULLUP);
+  pinMode(Botao_Pular, INPUT_PULLUP);
+}
 
 int main(void) {
   printf("Hello World\n");
