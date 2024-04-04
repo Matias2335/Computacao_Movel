1388a43930c75997a772f15b50574e9f4cacad9a
Matias2335 <113120652+Matias2335@users.noreply.github.com>
2024-04-04 04:01:02 +0000

    Arrumando o random de perguntas

--- main.cpp
+++ main.cpp
@@ -1,64 +1,74 @@
-#include "LiquidCrystal.h"
+#include <LiquidCrystal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <Tone.h>
 
-#define Botao_Iniciar 11;
-#define Botao_Sim 10;
-#define Botao_Nao 9;
-#define Botao_Pular 8;
-#define LED 12;
-#define TAMANHO_VETOR 16;
 
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+// Definições de pinos
+#define Botao_Iniciar 11
+#define Botao_Sim 10
+#define Botao_Nao 9
+#define Botao_Pular 8
+#define LED 12
+#define TAMANHO_VETOR 16
 
+// Buzzer
+int buzzer = 13;
+
+// Notas musicais
+int notes[] = {262, 294, 330, 349, 392, 440, 494, 523};
+
+// Melodias
+int correctMelody[] = {notes[4], notes[6], notes[7], notes[5], notes[4]};
+int errorMelody[] = {notes[0], notes[1], notes[2], notes[1], notes[0]};
+int winMelody[] = {notes[7], notes[6], notes[5], notes[4], notes[3], notes[2], notes[1]};
+
+// Perguntas
 typedef char Pergunta[100];
 Pergunta perguntas[16];
 
-void inserirPergunta(Pergunta pergunta, int indice) {
-  strcpy(perguntas[indice], pergunta);
-}
-
-void imprimirPerguntaAleatoria() {
-  int indiceAleatorio = random(TAMANHO_VETOR);
-  lcd.clear();
-  lcd.setCursor(0, 0);
-  lcd.print(perguntas[indiceAleatorio]);
-}
+// Funções
+void inserirPergunta(Pergunta pergunta, int indice);
+void imprimirPerguntaAleatoria();
+void tocarSom(int melodia[]);
 
 void setup() {
-  pinMode(Botao_Iniciar, INPUT_PULLUP);
-  pinMode(Botao_Sim, INPUT_PULLUP);
-  pinMode(Botao_Nao, INPUT_PULLUP);
-  pinMode(Botao_Pular, INPUT_PULLUP);
-  pinMode(LED, OUTPUT);
+  // Inicialização do LCD
+  LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
   lcd.begin(16, 2);
 
+  // Inicialização do Tone
+  tone(buzzer, notes[0]);
+  noTone(buzzer);
+
+  // Inserir perguntas
   //Facil
-  inserirPergunta("Tetris é um jogo de quebra-cabeça?"); // Resposta: Sim
-  inserirPergunta("Fortnite é um jogo de estratégia em tempo real?"); // Resposta: Não
-  inserirPergunta("Angry Birds foi desenvolvido pela EA Sports?"); // Resposta: Não
-  inserirPergunta("The Sims permite que os jogadores controlem um exército?"); // Resposta: Não
-  inserirPergunta("FIFA é uma série de jogos de corrida?"); // Resposta: Não
+  inserirPergunta("Tetris é um jogo de quebra-cabeça?", 1); // Sim
+  inserirPergunta("Fortnite é um jogo de estratégia em tempo real?", 2); // Não
+  inserirPergunta("Angry Birds foi desenvolvido pela EA Sports?", 3); // Não
+  inserirPergunta("The Sims permite que os jogadores controlem um exército?", 4); // Não
+  inserirPergunta("FIFA é uma série de jogos de corrida?", 5); // Não
 
+  
   //Medio
-  inserirPergunta("God of War é ambientado na mitologia grega?"); // Resposta: Sim
-  inserirPergunta("Overwatch é um jogo de tiro em primeira pessoa?"); // Resposta: Sim
-  inserirPergunta("World of Warcraft é um jogo de plataforma 2D?"); // Resposta: Não
-  inserirPergunta("Metal Gear Solid é conhecido por seu foco em combate direto?"); // Resposta: Não
-  inserirPergunta("Street Fighter é uma série de jogos de luta?"); // Resposta: Sim
-
+  inserirPergunta("God of War é ambientado na mitologia grega?", 6); // Sim
+  inserirPergunta("Overwatch é um jogo de tiro em primeira pessoa?", 7); // Sim
+  inserirPergunta("World of Warcraft é um jogo de plataforma 2D?", 8); // Não
+  inserirPergunta("Metal Gear Solid é conhecido por seu foco em combate direto?", 9); // Não
+  inserirPergunta("Street Fighter é uma série de jogos de luta?", 10); // Sim
   //Dificil
-  inserirPergunta("BioShock se passa em uma cidade subaquática chamada Rapture?"); // Resposta: Sim
-  inserirPergunta("Mass Effect é uma série de jogos de corrida?"); // Resposta: Não
-  inserirPergunta("Doom é creditado por ser um dos pioneiros do gênero battle royale?"); // Resposta: Não
-  inserirPergunta("Civilization é uma série de jogos de construção de cidades?"); // Resposta: Não
-  inserirPergunta("Final Fantasy é uma série de jogos de estratégia em tempo real?"); // Resposta: Não
+  inserirPergunta("BioShock se passa em uma cidade subaquática chamada Rapture?", 11); // Sim
+  inserirPergunta("Mass Effect é uma série de jogos de corrida?", 12); // Não
+  inserirPergunta("Doom é creditado por ser um dos pioneiros do gênero battle royale?", 13); // Não
+  inserirPergunta("Civilization é uma série de jogos de construção de cidades?", 14); // Sim
+  inserirPergunta("Final Fantasy é uma série de jogos de estratégia em tempo real?", 15); // Não
+  //
+  inserirPergunta("É possível terminar o jogo 'Dark Souls' sem sofrer nenhum dano?", 16); // Não
 
-  //Impossivel
-  inserirPergunta("É possível terminar o jogo 'Dark Souls' sem sofrer nenhum dano?"); // Resposta: Não
-
+  // Sementes aleatórias
   randomSeed(analogRead(0));
 
+  // Mensagem inicial
   lcd.clear();
   lcd.setCursor(0, 0);
   lcd.print("SEJA BEM VINDO");
@@ -68,31 +78,33 @@ void setup() {
 }
 
 void loop() {
-  while (digitalRead(Botao_Iniciar) == HIGH) {}
+          int nivel = random(0, 4); // Adicionei o nível "Impossível", então agora varia de 0 a 3
 
-  while (true) {
-    imprimirPerguntaAleatoria();
+          // Escolhe aleatoriamente uma questão do nível de dificuldade selecionado
+          int indice;
+          char* perguntaSelecionada;
+          char* respostaEsperada;
 
-    while (true) {
-
-      if (digitalRead(Botao_Sim) == LOW) {
-        digitalWrite(LED, HIGH);
-        delay(500);
-        digitalWrite(LED, LOW);
-        break;
-      }
-
-      if (digitalRead(Botao_Nao) == LOW) {
-        digitalWrite(LED, LOW);
-        delay(500);
-        break;
-      }
-
-      if (digitalRead(Botao_Pular) == LOW) {
-        break;
-      }
-    }
-
-    delay(1000);
-  }
-}
+          switch (nivel) {
+            case 0: // Fácil
+              indice = random(0, 5);
+              perguntaSelecionada = perguntasFaceis[indice];
+              respostaEsperada = &respostasFaceis[indice];
+              break;
+            case 1: // Médio
+              indice = random(0, 5);
+              perguntaSelecionada = perguntasMedias[indice];
+              respostaEsperada = &respostasMedias[indice];
+              break;
+            case 2: // Difícil
+              indice = random(0, 5);
+              perguntaSelecionada = perguntasDificeis[indice];
+              respostaEsperada = &respostasDificeis[indice];
+              break;
+            case 3: // Impossível
+              indice = 0; // Apenas uma pergunta no nível "Impossível"
+              perguntaSelecionada = perguntaImpossivel[indice];
+              respostaEsperada = &respostasImpossivel[indice];
+              break;
+          }
+        }
