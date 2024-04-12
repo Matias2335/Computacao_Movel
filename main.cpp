216ca8e28f06d86784912725ec03db16bdcafced
Matias2335 <113120652+Matias2335@users.noreply.github.com>
2024-04-12 23:18:18 +0000

    final ja testado

--- main.cpp
+++ main.cpp
@@ -5,23 +5,29 @@ LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int Botao_Sim = 0;
 int Botao_Nao = 0;
 int Botao_Pular = 0;
-int Botao_Desistir = 0; // Botão para desistir do jogo na porta 7
+int Botao_Desistir = 0; 
 int score = 0;
 int tentativas = 3;
-int numeroQuestao = 1; // Variável para controlar o número da questão atual
+int numeroQuestao = 1;
 unsigned long startTime = 0;
-const int tempoTotal = 15000; // Aumentei o tempo total para 15 segundos
+const int tempoTotal = 15000;
 const int ledPin = 6;
-const int buzzerPin = 14; // Pino do buzzer
+const int buzzerPin = A5; //buzzer
+
+// Frequências para os sons
+const int freqAcerto = 1000;
+const int freqErro = 500;     
+const int freqPulo = 300; 
+const int freqDesistencia = 200; 
 
 // Arrays para controlar quais perguntas já foram feitas
 bool perguntasFeitasFaceis[5] = {false};
 bool perguntasFeitasMedias[5] = {false};
 bool perguntasFeitasDificeis[5] = {false};
-bool perguntaFeitaImpossivel = false;
+bool perguntaFeitaImpossivel = {false};
 
-// Array de perguntas e respostas fáceis
-char perguntasFaceis[5][72] = {
+// Arrays de perguntas e respostas
+char* perguntasFaceis[5] = {
   "Tetris eh um jogo de quebra cabeca?",
   "Fortnite eh um jogo de estrategia em tempo real?",
   "Angry Birds foi desenvolvido pela EA Sports?",
@@ -29,48 +35,35 @@ char perguntasFaceis[5][72] = {
   "FIFA eh uma serie de jogos de corrida?"
 };
 
-char respostasFaceis[5] = {'S', 'N', 'N', 'N', 'N'}; // Respostas correspondentes às perguntas fáceis
+char respostasFaceis[5] = {'S', 'N', 'N', 'N', 'N'}; //Resposta Facil
 
-// Array de perguntas e respostas médias
-char perguntasMedias[5][72] = {
+char* perguntasMedias[5] = {
   "God of War eh ambientado na mitologia grega?",
   "Overwatch eh um jogo de tiro em primeira pessoa?",
   "World of Warcraft eh um jogo de plataforma 2D?",
   "Metal Gear Solid eh conhecido por seu foco em combate direto?",
   "Street Fighter eh uma serie de jogos de luta?"
-};
+}; 
 
-char respostasMedias[5] = {'S', 'S', 'N', 'N', 'S'}; // Respostas correspondentes às perguntas médias
+char respostasMedias[5] = {'S', 'S', 'N', 'N', 'S'}; // Respostas médias
 
-// Array de perguntas e respostas difíceis
-char perguntasDificeis[5][72] = {
+char* perguntasDificeis[5] = {
   "BioShock se passa em uma cidade subaquatica chamada Rapture?",
   "Mass Effect eh uma serie de jogos de corrida?",
   "Doom eh creditado por ser um dos pioneiros do genero battle royale?",
   "Civilization eh uma serie de jogos de construcao de cidades?",
-  "Final Fantasy eh uma série de jogos de estrategia em tempo real?"
+  "Final Fantasy eh uma serie de jogos de estrategia em tempo real?"
 };
 
-char respostasDificeis[5] = {'S', 'N', 'N', 'S', 'N'}; // Respostas correspondentes às perguntas difíceis
+char respostasDificeis[5] = {'S', 'N', 'N', 'S', 'N'}; // Respostas difíceis
 
-// Array de perguntas e respostas impossíveis
-char perguntaImpossivel[1][72] = {
+char* perguntaImpossivel[1] = {
   "Eh possivel terminar o jogo 'Dark Souls' sem sofrer nenhum dano?"
 };
 
-char respostasImpossivel[1] = {'N'};
+char respostasImpossivel[1] = {'S'};
 
-// Frequências para os sons
-const int freqAcerto = 1000; // Frequência para acerto
-const int freqErro = 500;     // Frequência para erro
-const int freqPulo = 300;     // Frequência para pulo
-const int freqDesistencia = 200; // Frequência para desistência
-
-// Melodia de vitória
-int victoryMelody[] = {262, 330, 392, 523, 659, 784, 1046};
-int victoryMelodyDurations[] = {400, 400, 400, 400, 400, 400, 800};
-
-// Função para reproduzir uma única nota no buzzer
+// Funções para tocar sons
 void playSound(int frequency, int duration) {
   tone(buzzerPin, frequency);
   delay(duration);
@@ -79,6 +72,9 @@ void playSound(int frequency, int duration) {
 
 // Função para tocar a melodia de vitória
 void playVictoryMelody() {
+  int victoryMelody[] = {262, 330, 392, 523, 659, 784, 1046};
+  int victoryMelodyDurations[] = {400, 400, 400, 400, 400, 400, 800};
+
   for (int i = 0; i < sizeof(victoryMelody) / sizeof(int); i++) {
     tone(buzzerPin, victoryMelody[i]);
     delay(victoryMelodyDurations[i]);
@@ -91,63 +87,61 @@ void setup() {
   lcd.cursor();
   pinMode(10, INPUT_PULLUP);
   pinMode(9, INPUT_PULLUP);
-  pinMode(8, INPUT_PULLUP); // Botão para pular a pergunta
-  pinMode(7, INPUT_PULLUP); // Botão para desistir do jogo
+  pinMode(8, INPUT_PULLUP); 
+  pinMode(7, INPUT_PULLUP); 
   pinMode(ledPin, OUTPUT);
   pinMode(buzzerPin, OUTPUT);
   Serial.begin(9600);
-  randomSeed(analogRead(0)); // Inicializa o gerador de números aleatórios
+  randomSeed(analogRead(0)); 
 }
 
 void loop() {
-  // Escolhe aleatoriamente um nível de dificuldade (0 para fácil, 1 para médio, 2 para difícil, 3 para impossível)
-  int nivel;
-  int indice;
   char* perguntaSelecionada;
   char* respostaEsperada;
 
-  do {
-    nivel = random(0, 4);
+  // Verifica se todas as perguntas de cada nível foram feitas
+  bool todasPerguntasFaceisFeitas = perguntasFeitasFaceis[0] && perguntasFeitasFaceis[1] && perguntasFeitasFaceis[2] && perguntasFeitasFaceis[3] && perguntasFeitasFaceis[4];
+  bool todasPerguntasMediasFeitas = perguntasFeitasMedias[0] && perguntasFeitasMedias[1] && perguntasFeitasMedias[2] && perguntasFeitasMedias[3] && perguntasFeitasMedias[4];
+  bool todasPerguntasDificeisFeitas = perguntasFeitasDificeis[0] && perguntasFeitasDificeis[1] && perguntasFeitasDificeis[2] && perguntasFeitasDificeis[3] && perguntasFeitasDificeis[4];
 
-    switch (nivel) {
-      case 0: // Fácil
-        indice = random(0, 5);
-        if (!perguntasFeitasFaceis[indice]) {
-          perguntasFeitasFaceis[indice] = true;
-          perguntaSelecionada = perguntasFaceis[indice];
-          respostaEsperada = &respostasFaceis[indice];
-        }
-        break;
-      case 1: // Médio
-        indice = random(0, 5);
-        if (!perguntasFeitasMedias[indice]) {
-          perguntasFeitasMedias[indice] = true;
-          perguntaSelecionada = perguntasMedias[indice];
-          respostaEsperada = &respostasMedias[indice];
-        }
-        break;
-      case 2: // Difícil
-        indice = random(0, 5);
-        if (!perguntasFeitasDificeis[indice]) {
-          perguntasFeitasDificeis[indice] = true;
-          perguntaSelecionada = perguntasDificeis[indice];
-          respostaEsperada = &respostasDificeis[indice];
-        }
-        break;
-      case 3: // Impossível
-        if (!perguntaFeitaImpossivel) {
-          perguntaFeitaImpossivel = true;
-          indice = 0;
-          perguntaSelecionada = perguntaImpossivel[indice];
-          respostaEsperada = &respostasImpossivel[indice];
-        }
-        break;
-    }
+  // Escolhe aleatoriamente uma pergunta do nível fácil, se ainda houver perguntas disponíveis nesse nível
+  if (!todasPerguntasFaceisFeitas) {
+    int indiceFacil;
+    do {
+      indiceFacil = random(0, 5);
+    } while (perguntasFeitasFaceis[indiceFacil]);
 
-    if (perguntaSelecionada != NULL) {
-      break; // Sai do loop do-while se uma pergunta válida foi selecionada
-    }
-  } while (true);
+    perguntasFeitasFaceis[indiceFacil] = true;
+    perguntaSelecionada = perguntasFaceis[indiceFacil];
+    respostaEsperada = &respostasFaceis[indiceFacil];
+  }
+  // Escolhe aleatoriamente uma pergunta do nível médio, se ainda houver perguntas disponíveis nesse nível
+  else if (!todasPerguntasMediasFeitas) {
+    int indiceMedio;
+    do {
+      indiceMedio = random(0, 5);
+    } while (perguntasFeitasMedias[indiceMedio]);
+
+    perguntasFeitasMedias[indiceMedio] = true;
+    perguntaSelecionada = perguntasMedias[indiceMedio];
+    respostaEsperada = &respostasMedias[indiceMedio];
+  }
+  // Escolhe aleatoriamente uma pergunta do nível difícil, se ainda houver perguntas disponíveis nesse nível
+  else if (!todasPerguntasDificeisFeitas) {
+    int indiceDificil;
+    do {
+      indiceDificil = random(0, 5);
+    } while (perguntasFeitasDificeis[indiceDificil]);
+
+    perguntasFeitasDificeis[indiceDificil] = true;
+    perguntaSelecionada = perguntasDificeis[indiceDificil];
+    respostaEsperada = &respostasDificeis[indiceDificil];
+  }
+  // Se todas as perguntas de todos os níveis foram feitas, escolhe a pergunta impossível
+  else {
+    perguntaSelecionada = perguntaImpossivel[0];
+    respostaEsperada = &respostasImpossivel[0];
+  }
 
   lcd.clear();
   lcd.write("Questao ");
@@ -156,37 +150,28 @@ void loop() {
   delay(3000);
   lcd.clear();
   lcd.home();
-
   int comprimento = strlen(perguntaSelecionada);
-
   if (comprimento > 16) {
-    lcd.clear(); // Limpa o LCD
+    lcd.clear(); 
     for (int i = 0; i < comprimento; i++) {
-        if (i == 16) {
-            lcd.clear(); // Limpa o LCD após 16 caracteres
-            lcd.setCursor(1, 0); // Vai para a segunda linha
-        } else if (i == 32) {
-            lcd.clear(); // Limpa o LCD após 32 caracteres
-            lcd.setCursor(1, 0); // Volta para a primeira linha
-        } else if (i == 48) {
-            lcd.clear(); // Limpa o LCD após 48 caracteres
-            lcd.setCursor(1,0); // Vai para a segunda linha
-        }else if (i ==64){
-          lcd.clear();
-            lcd.setCursor(1,0);
-      }
+        if (i == 16 || i == 32 || i == 48 || i == 64) {
+            lcd.clear();
+            lcd.setCursor(0, 0); 
+        }
         lcd.write(perguntaSelecionada[i]);
         delay(250);
     }
-} else {
+  } else {
     lcd.print(perguntaSelecionada);
-}
+  }
 
+  // Atualiza o valor de comprimento após a exibição da pergunta
+  comprimento = strlen(perguntaSelecionada);
 
   delay(2000);
   lcd.clear();
   lcd.write("Q-");
-  lcd.print(numeroQuestao); // Mostra o número da questão atual
+  lcd.print(numeroQuestao);
   lcd.setCursor(8, 0);
   lcd.write("Score=");
   lcd.print(score);
@@ -233,70 +218,65 @@ void loop() {
     lcd.write(" Vidas");
     if (tentativas == 0) {
       lcd.clear();
+      playSound(freqDesistencia, 200);
       lcd.write("Fim do Jogo!");
       lcd.setCursor(3,1);
       lcd.write("Score=");
       lcd.print(score);
       delay(5000);
-      while (true) {
-        // Loop infinito para encerrar o jogo
+          while (true) {
+    
       }
     }
-    playSound(freqPulo, 200); // Reproduz o som de pulo
+    playSound(freqPulo, 200);
   } else if (Botao_Desistir == HIGH || tentativas == 0) {
-    playSound(freqDesistencia, 200); // Reproduz o som de desistência
+    playSound(freqDesistencia, 200);
     lcd.write("Fim do Jogo!");
     lcd.setCursor(0, 1);
     lcd.write("Score=");
     lcd.print(score);
     delay(5000);
     while (true) {
-      // Loop infinito para encerrar o jogo
+  
     }
   } else {
     if ((Botao_Sim == HIGH && *respostaEsperada == 'S') || (Botao_Nao == HIGH && *respostaEsperada == 'N')) {
       score++;
-      // Adicionei um bônus de pontuação por resposta correta
-      score += 2; // Adicionei um bônus extra por resposta rápida
-      lcd.write("Resposta Correta! +2 pontos!");
-      playSound(freqAcerto, 200); // Reproduz o som de acerto
+      lcd.write("Resposta Correta!");
+      playSound(freqAcerto, 200);
     } else {
       tentativas--;
-      // Reduza o número de tentativas por resposta errada
-      score -= 1; // Aplique uma penalidade por resposta errada
-      lcd.write("Resposta Errada! -1 ponto");
+      lcd.write("Resposta Errada!");
       // Verifica se o usuário ainda tem vidas restantes
       if (tentativas == 0) {
         lcd.clear();
+        playSound(freqDesistencia, 200);
         lcd.write("Fim do Jogo!");
         lcd.setCursor(3,1);
         lcd.write("Score=");
         lcd.print(score);
         delay(5000);
         while (true) {
-          // Loop infinito para encerrar o jogo
         }
       }
-      playSound(freqErro, 200); // Reproduz o som de erro
+      playSound(freqErro, 200);
     }
   }
 
   delay(5000);
-
-  // Incrementa o número da questão para pular para a próxima
+  
   numeroQuestao++;
 
   // Verifica se o jogo foi vencido (todas as perguntas respondidas corretamente)
-  if (numeroQuestao > 5 && tentativas > 0) {
+  if (numeroQuestao > 16 && tentativas > 0) {
     lcd.clear();
     lcd.write("Voce venceu!");
     lcd.setCursor(0, 1);
     lcd.write("Score=");
     lcd.print(score);
     delay(5000);
-    playVictoryMelody(); // Toca a melodia de vitória
+    playVictoryMelody();
     while (true) {
-      // Loop infinito para manter a exibição da mensagem de vitória
     }
   }
 }
